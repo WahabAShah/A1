72062b29992deff51026b5786ccec7b51b87ae49
wahab <wahab@DESKTOP-6606C4Q>
2019-10-16 18:41:12 -0400

    Milestone 1: Project Initialization Fixed, MVC in startup.cs

---
+++ A1/Models/ErrorViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace A1.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; internal set; }
+    }
+}
